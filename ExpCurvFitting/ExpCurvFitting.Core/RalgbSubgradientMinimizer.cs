6312d036939bf0b31e127c3d3746b316af2537f0
Max Zvy <zvyagin.maksim@inbox.ru>
2024-08-03 21:58:47 +0700

    init

---
+++ ExpCurvFitting/ExpCurvFitting.Core/RalgbSubgradientMinimizer.cs
@@ -0,0 +1,68 @@
+﻿using MathNet.Numerics.LinearAlgebra;
+using MathNet.Numerics.Optimization;
+using MathNet.Numerics.Optimization.LineSearch;
+
+namespace ExpCurvFitting.Core
+{
+    internal class RalgbSubgradientMinimizer : IUnconstrainedMinimizer
+    {
+        public double GradientTolerance { get; set; }
+        public int MaximumIterations { get; set; }
+
+        public RalgbSubgradientMinimizer(double gradientTolerance, int maximumIterations)
+        {
+            GradientTolerance = gradientTolerance;
+            MaximumIterations = maximumIterations;
+        }
+
+        public MinimizationResult FindMinimum(IObjectiveFunction objective, Vector<double> initialGuess)
+        {
+            return Minimum(objective, initialGuess, GradientTolerance, MaximumIterations);
+        }
+
+        public static MinimizationResult Minimum(IObjectiveFunction objective, Vector<double> initialGuess, double gradientTolerance = 1e-8, int maxIterations = 1000)
+        {
+            if (!objective.IsGradientSupported)
+            {
+                throw new IncompatibleObjectiveException("Gradient not supported in objective function, but required for ConjugateGradient minimization.");
+            }
+
+            objective.EvaluateAt(initialGuess);
+            var gradient = objective.Gradient;
+            ValidateGradient(objective);
+
+            // Check that we're not already done
+            if (gradient.Norm(2.0) < gradientTolerance)
+            {
+                return new MinimizationResult(objective, 0, ExitCondition.AbsoluteGradient);
+            }
+
+            
+            return new MinimizationWithLineSearchResult(objective, 0, ExitCondition.AbsoluteGradient, totalLineSearchSteps, iterationsWithNontrivialLineSearch);
+        }
+
+        static void ValidateGradient(IObjectiveFunctionEvaluation objective)
+        {
+            foreach (var x in objective.Gradient)
+            {
+                if (double.IsNaN(x) || double.IsInfinity(x))
+                {
+                    throw new EvaluationException("Non-finite gradient returned.", objective);
+                }
+            }
+        }
+
+        static void ValidateObjective(IObjectiveFunctionEvaluation objective)
+        {
+            if (double.IsNaN(objective.Value) || double.IsInfinity(objective.Value))
+            {
+                throw new EvaluationException("Non-finite objective function returned.", objective);
+            }
+        }
+
+        public MinimizationResult FindMinimum(IObjectiveFunction objective, Vector<double> initialGuess)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ExpCurvFitting/ExpCurvFitting.Core/Tol.cs
@@ -0,0 +1,44 @@
+﻿using MathNet.Numerics.LinearAlgebra;
+
+namespace ExpCurvFitting.Core;
+public record Tol
+{
+    public Vector<double> XLowerBound { get; set; }
+    public Vector<double> XUpperBound { get; set; }
+    public Vector<double> YLowerBound { get; set; }
+    public Vector<double> YUpperBound { get; set; }
+
+    public Vector<double> XMid => 0.5 * (XLowerBound + XUpperBound);
+    public Vector<double> XRad => 0.5 * (XUpperBound - XLowerBound);
+    public Vector<double> YMid => 0.5 * (YLowerBound + YUpperBound);
+    public Vector<double> YRad => 0.5 * (YUpperBound - YLowerBound);
+    public Tol(Vector<double> xLowerBound, Vector<double> xUpperBound, Vector<double> yLowerBound, Vector<double> yUpperBound)
+    {
+        XLowerBound = xLowerBound;
+        XUpperBound = xUpperBound;
+        YLowerBound = yLowerBound;
+        YUpperBound = yUpperBound;
+    }
+
+    public double TolValue(Vector<double> a, Vector<double> b)
+    {
+        return 0;
+    }
+
+    public Vector<double> GradA(Vector<double> a, Vector<double> b)
+    {
+        return null;
+    }
+    public Vector<double> GradB(Vector<double> a, Vector<double> b)
+    {
+        return null;
+    }
+    public Vector<double> Grad(Vector<double> x0)
+    {
+        return null;
+    }
+    public double TolValue(Vector<double> x0)
+    {
+        return 0;
+    }
+}
